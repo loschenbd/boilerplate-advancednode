6163a12e47c6338b3a027d76f36b4ad847523d40
Benjamin Loschen <loschenbd@gmail.com>
2020-10-08 09:00:06 -0500

    Initial Commit. Added Pug. Set view engine.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require("express");
 const myDB = require('./connection');
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const pug = require('pug');
 
 const app = express();
 
@@ -11,9 +12,12 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'pug')
+app.set('views', './views/pug')
+
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
-  res.render('Load your view here');
+  res.render('index');
 });
 
 app.listen(process.env.PORT || 3000, () => {
